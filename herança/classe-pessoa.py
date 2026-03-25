a42913bb29ce2977dcc5b695b290b5b18f39950a
Isadoraalcantara <Isadoraalca405@gmail.com>
2026-03-25 09:56:56 -0300

    Atividade herança: classe-pessoa e classe-veiculo

---
+++ herança/classe-pessoa.py
@@ -0,0 +1,20 @@
+class Pessoa:
+
+    def __init__(self, nome, idade):
+        self.nome = nome
+        self.idade = idade
+
+    def getPessoa(self):
+        return(f"Nome pessoa: {self.nome}, idade pessoa: {self.idade}")
+
+class Aluno(Pessoa):
+
+    def __init__(self, nome, idade, nota):
+        super().__init__(nome, idade)
+        self.nota = nota
+
+    def getAluno(self):
+        return(f"Nome aluno: {self.nome}, idade aluno: {self.idade}, nota aluno: {self.nota}")
+    
+pessoa1 = Pessoa("Ana", 23)
+aluno1 = Aluno
