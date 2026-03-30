8e142dccbc3eff6d45d6208a144599df0112822b
Isadoraalcantara <Isadoraalca405@gmail.com>
2026-03-30 11:37:25 -0300

    Atividade 4: classe-forma

---
+++ herança/classe-forma.py
@@ -0,0 +1,18 @@
+NAO FAZ SENTIDO
+
+class Forma:
+
+    def __init__(self):
+        pass
+
+    def area(self):
+        pass
+
+class Retangulo(Forma):
+
+    def __init__(self, comprimento, largura):
+        super().__init__(self)
+        self.comprimento = comprimento
+        self.largura = largura
+
+    def 
