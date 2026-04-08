0b0ab9b2ad29ed8a08bc7a2d909b36be712e73e8
IsadoraAlcantara <IsadoraAlca405@gmail.com>
2026-04-08 09:39:07 -0300

    Atividade 5: classe-forma

---
+++ polimorfismo/classe-forma.py
@@ -0,0 +1,32 @@
+from math import pi
+
+class Forma:
+
+    def __init__(self, tipoForma):
+        self.tipoForma = tipoForma
+
+    def area(self):
+        pass
+
+class Quadrado(Forma):
+
+    def __init__(self, lado):
+        super().__init__("quadrado")
+        self.lado = lado
+
+    def area(self):
+        areaTotal = self.lado ** 2
+        return f"O {self.tipoForma} tem {areaTotal} cm² de área"
+
+class Circulo(Forma):
+
+    def __init__(self, raio):
+        super().__init__("círculo")
+        self.raio = raio
+
+    def area(self):
+        areaTotal = pi * self.raio ** 2
+        return f"O {self.tipoForma} tem {round(areaTotal, 2)} cm² de área"
+    
+for forma in (Quadrado(2), Circulo(5)):
+    print(forma.area())
