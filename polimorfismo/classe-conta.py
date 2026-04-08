7c1f4901656fbe67d37e2689a302f94cd7440dbd
IsadoraAlcantara <IsadoraAlca405@gmail.com>
2026-04-08 10:06:06 -0300

    Atividade 5: classe-conta

---
+++ polimorfismo/classe-conta.py
@@ -0,0 +1,14 @@
+class ContaBancaria:
+
+    def __init__(self, nomeCliente, valorConta, meses):
+        self.nomeCliente = nomeCliente
+        self.valorConta = valorConta
+        self.meses = meses
+
+    def deposito(self,):
+        pass
+
+    def retirada(self):
+        pass
+
+# Formula juros simples - M = c * (1 + i * t)
